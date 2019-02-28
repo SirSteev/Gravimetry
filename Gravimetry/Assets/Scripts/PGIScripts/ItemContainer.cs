a875eb7992c838e6faee48279ef30d343aac51d7
SirSteev <37564104+SirSteev@users.noreply.github.com>
2019-02-27 22:17:23 -0500

    more tweaks to inventory
    only one bug with rotating item and moving it between containers neew to find a way to get the view of the container the item came from to see if the item was rotated while dragging it. other than that started assembling the inventory menu and playing around.

---
+++ Gravimetry/Assets/Scripts/PGIScripts/ItemContainer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PowerGridInventory;
+
+public class ItemContainer : MonoBehaviour
+{
+    public PGIView containerGridView;
+    public PGISlot containerSlot;
+
+    public void OnEquip()
+    {
+        PGIModel model = containerSlot.Item.gameObject.GetComponent<PGIModel>();
+        containerGridView.Model = model;
+    }
+
+    public void OnUnEquip()
+    {
+        containerGridView.Model = null;
+    }
+}
